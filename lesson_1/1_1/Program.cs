3b51e48c7601d2a828a74dc06b155a2e137fdd8d
Roman <pliskarv@yandex.ru>
2024-01-05 16:41:18 +0900

    завешили_задачу_1

---
+++ lesson_1/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+
+int a = -3;
+int b = 9;
+if (b * b == a)
+    Console.WriteLine ("true");
+else
+    Console.WriteLine ("false");
