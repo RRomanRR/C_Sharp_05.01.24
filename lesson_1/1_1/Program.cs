8107f2134fff08afff76dbf819ac76494eeb6706
Roman <pliskarv@yandex.ru>
2024-01-05 17:01:59 +0900

    создали 1_2

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -3,9 +3,13 @@
 // Console.WriteLine("Hello, World!");
 
 
-int a = -3;
-int b = 9;
-if (b * b == a)
-    Console.WriteLine ("true");
-else
-    Console.WriteLine ("false");
+// int a = -3;
+// int b = 9;
+// if (b * b == a)
+// Console.WriteLine ("true");
+// else
+// Console.WriteLine ("false");
+
+Console.Write("Enter a number: ");
+int num1 = int.Parse(Console.ReadLine ( )!);
+Console.WriteLine(num1 + num1);
